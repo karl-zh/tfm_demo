0aa73cd926d40785e82f6e8d760dbca22b54dfc0
Karl Zhang <karl.zhang@linaro.org>
2019-05-25 20:57:36 +0800

    compile the openamp and libmetal lib into tfm initially

--- secure_fw/openAMP/tfm_openamp.c
+++ secure_fw/openAMP/tfm_openamp.c
@@ -31,8 +31,99 @@ static struct metal_device shm_device = {
 	.irq_info = NULL
 };
 
+static volatile unsigned int received_data;
 
+static struct virtio_vring_info rvrings[2] = {
+	[0] = {
+		.info.align = VRING_ALIGNMENT,
+	},
+	[1] = {
+		.info.align = VRING_ALIGNMENT,
+	},
+};
+static struct virtio_device vdev;
+static struct rpmsg_virtio_device rvdev;
 static struct metal_io_region *io;
+static struct virtqueue *vq[2];
+
+static unsigned char virtio_get_status(struct virtio_device *vdev)
+{
+//	return VIRTIO_CONFIG_STATUS_DRIVER_OK;
+}
+
+static void virtio_set_status(struct virtio_device *vdev, unsigned char status)
+{
+//	sys_write8(status, VDEV_STATUS_ADDR);
+}
+
+static uint32_t virtio_get_features(struct virtio_device *vdev)
+{
+//	return 1 << VIRTIO_RPMSG_F_NS;
+}
+
+static void virtio_set_features(struct virtio_device *vdev,
+				uint32_t features)
+{
+}
+
+static void virtio_notify(struct virtqueue *vq)
+{
+//	uint32_t dummy_data = 0x55005500; /* Some data must be provided */
+
+//	ipm_send(ipm_handle, 0, 0, &dummy_data, sizeof(dummy_data));
+}
+
+struct virtio_dispatch dispatch = {
+	.get_status = virtio_get_status,
+	.set_status = virtio_set_status,
+	.get_features = virtio_get_features,
+	.set_features = virtio_set_features,
+	.notify = virtio_notify,
+};
+
+//static K_SEM_DEFINE(data_sem, 0, 1);
+//static K_SEM_DEFINE(data_rx_sem, 0, 1);
+
+//static void platform_ipm_callback(void *context, uint32_t id, volatile void *data)
+//{
+//	LOG_MSG(__func__);
+//	k_sem_give(&data_sem);
+//}
+
+int endpoint_cb(struct rpmsg_endpoint *ept, void *data,
+		size_t len, uint32_t src, void *priv)
+{
+	LOG_MSG(__func__);
+//	received_data = *((unsigned int *) data);
+
+//	k_sem_give(&data_rx_sem);
+
+	return RPMSG_SUCCESS;
+}
+
+//static K_SEM_DEFINE(ept_sem, 0, 1);
+
+struct rpmsg_endpoint my_ept;
+struct rpmsg_endpoint *ep = &my_ept;
+
+static void rpmsg_service_unbind(struct rpmsg_endpoint *ept)
+{
+	(void)ept;
+	rpmsg_destroy_ept(ep);
+}
+
+void ns_bind_cb(struct rpmsg_device *rdev, const char *name, uint32_t dest)
+{
+	(void)rpmsg_create_ept(ep, rdev, name,
+			RPMSG_ADDR_ANY, dest,
+			endpoint_cb,
+			rpmsg_service_unbind);
+
+	LOG_MSG(__func__);
+//	k_sem_give(&ept_sem);
+}
+
+static struct rpmsg_virtio_shm_pool shpool;
 
 void tfm_openamp_init(void)
 {
@@ -66,31 +157,16 @@ void tfm_openamp_init(void)
 		LOG_MSG("metal_device_io_region failed to get region\n");
 		return;
 	}
-#if 0
-	/* setup IPM */
-	ipm_handle = device_get_binding("MAILBOX_0");
-	if (ipm_handle == NULL) {
-		printk("device_get_binding failed to find device\n");
-		return;
-	}
-
-	ipm_register_callback(ipm_handle, platform_ipm_callback, NULL);
-
-	status = ipm_set_enabled(ipm_handle, 1);
-	if (status != 0) {
-		printk("ipm_set_enabled failed\n");
-		return;
-	}
 
 	/* setup vdev */
 	vq[0] = virtqueue_allocate(VRING_SIZE);
 	if (vq[0] == NULL) {
-		printk("virtqueue_allocate failed to alloc vq[0]\n");
+		LOG_MSG("virtqueue_allocate failed to alloc vq[0]\n");
 		return;
 	}
 	vq[1] = virtqueue_allocate(VRING_SIZE);
 	if (vq[1] == NULL) {
-		printk("virtqueue_allocate failed to alloc vq[1]\n");
+		LOG_MSG("virtqueue_allocate failed to alloc vq[1]\n");
 		return;
 	}
 
@@ -115,8 +191,9 @@ void tfm_openamp_init(void)
 	rpmsg_virtio_init_shm_pool(&shpool, (void *)SHM_START_ADDR, SHM_SIZE);
 	status = rpmsg_init_vdev(&rvdev, &vdev, ns_bind_cb, io, &shpool);
 	if (status != 0) {
-		printk("rpmsg_init_vdev failed %d\n", status);
+		LOG_MSG("rpmsg_init_vdev failed\n");
 		return;
 	}
-#endif
+	LOG_MSG("rpmsg_init_vdev Done!\n");
+
 }
