5fe48cac1699604ee55bb274cfa394ca33381696
Karl Zhang <karl.zhang@linaro.org>
2019-06-25 18:22:28 +0800

    eRPC: Add eRPC services
    TFM act as eRPC server and waiting for requests all the time. all
    services shim are from eRPC library. What the TFM todo in secure is
    provide the implementation of each function that required from eRPC
    clients.
    
    Change-Id: Icdd2277763364e1d882a4142077b98ed34a4be3d
    Signed-off-by: Karl Zhang <karl.zhang@linaro.org>

--- secure_fw/eRPC/tfm_erpc.c
+++ secure_fw/eRPC/tfm_erpc.c
@@ -11,6 +11,45 @@
 #include "erpc_mbf_setup.h"
 #include "erpc_transport_setup.h"
 #include "erpc_server_setup.h"
+#include "secure_utilities.h"
+
+/*!
+* @brief erpcMatrixMultiply function implementation.
+ *
+ * This is the implementation of the erpcMatrixMultiply function called by the primary core.
+ *
+ * @param matrix1 First matrix
+ * @param matrix2 Second matrix
+ * @param result_matrix Result matrix
+ */
+void erpcMatrixMultiply(Matrix matrix1, Matrix matrix2, Matrix result_matrix)
+{
+    int32_t i, j, k;
+    const int32_t matrix_size = 5;
+
+    /* Clear the result matrix */
+    for (i = 0; i < matrix_size; ++i)
+    {
+        for (j = 0; j < matrix_size; ++j)
+        {
+           result_matrix[i][j] = 0;
+        }
+    }
+
+    /* Multiply two matrices */
+    for (i = 0; i < matrix_size; ++i)
+    {
+        for (j = 0; j < matrix_size; ++j)
+        {
+            for (k = 0; k < matrix_size; ++k)
+            {
+                result_matrix[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    printf("Matrix Done!\r\n");
+}
 
 void tfm_erpc_init(struct rpmsg_endpoint *ep)
 {
@@ -18,6 +57,16 @@ void tfm_erpc_init(struct rpmsg_endpoint *ep)
     void * message_buffer_factory = erpc_mbf_static_init();
 
     erpc_server_init(transport ,message_buffer_factory);
+
+    /* adding the service to the server */
+    erpc_add_service_to_server(create_MatrixMultiplyService_service());
+    erpc_add_service_to_server(create_PsaFrameworkVersionService_service());
+    erpc_add_service_to_server(create_PsaVersionService_service());
+    erpc_add_service_to_server(create_PsaConnectService_service());
+    erpc_add_service_to_server(create_PsaCallService_service());
+    erpc_add_service_to_server(create_PsaCloseService_service());
+
+    printf("eRPC services added\r\n");
 }
 
 void tfm_erpc_poll()
